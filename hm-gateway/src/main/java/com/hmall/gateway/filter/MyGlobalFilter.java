7dfb13facd5fc06ced4d262114537e4a1d7b79a3
zhuxx <zhuxiaoxia0215@qq.com>
2024-06-10 17:50:18 +0800

    网关登录校验

--- hm-gateway/src/main/java/com/hmall/gateway/filter/MyGlobalFilter.java
+++ hm-gateway/src/main/java/com/hmall/gateway/filter/MyGlobalFilter.java
@@ -5,9 +5,11 @@ import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+@Component
 public class MyGlobalFilter implements GlobalFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
